0829ce5477fbcb1b8d3a2fd6df71e53d480f0c80
Oren Sharizad <oren.sharizad12@gmail.com>
2022-12-28 13:03:32 +0200

    first render of notes

--- apps/note/views/note-index.jsx
+++ apps/note/views/note-index.jsx
@@ -1,6 +1,28 @@
+import { NoteList } from "../cmps/note-list.jsx"
+import { NoteService } from "../services/note.service.js"
+
+const { useState, useEffect } = React
+const { Link } = ReactRouterDOM
+
 
 export function NoteIndex() {
 
-    return <div>note app</div>
+    const [notes, setNotes] = useState([])
+    useEffect(() => {
+        loadNotes()
+    }, [])
+
+
+    function loadNotes() {
+        NoteService.query().then(notes => {
+            console.log(notes);
+            setNotes(notes)
+        })
+    }
+    if (!notes) return <div>loading...</div>
+    return <section className="note-index">
+        <NoteList notes={notes} />
+
+    </section>
 
 }
